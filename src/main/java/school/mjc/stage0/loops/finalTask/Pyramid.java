c4eb55c6acddbab02c551ae3624011ff5efe2aeb
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-08-27 11:05:24 +0500

    Update Pyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,28 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        StringBuilder copy = new StringBuilder();
+        StringBuilder s = new StringBuilder();
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength; j++) {
+                if(cathetusLength - i >= j){
+                    s.append(" ");
+                }
 
+            }
+            if(i != 1){
+                copy.append(i);
+            }
+            if(i != 1){
+                s.append(copy.reverse());
+                s.append(1);
+                s.append(copy.reverse());
+            }
+            else
+                s.append(1);
+            s.append("\n");
+        }
+        System.out.println(s);
     }
 
     public static void main(String[] args) {
